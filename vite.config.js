0a70405f01979aae52f89f6cf7f453ac99cedb66
andi-mache <amache75@gmail.com>
2023-07-15 15:33:03 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: 'localhost',
+   },
+
     plugins: [
         laravel([
             'resources/css/app.css',
